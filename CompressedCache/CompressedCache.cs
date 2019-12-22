527a5f2760e351938c9377d7b0816e90924edba0
deepanjanbhol <40578082+deepanjanbhol@users.noreply.github.com>
2019-12-21 18:39:44 -0800

    Added compressed cache
    Added compressed cache

---
+++ CompressedCache/CompressedCache.cs
@@ -0,0 +1,77 @@
+﻿
+namespace CompressedCache
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Compressed cache using GZIP.
+    /// Keeps the values in compressed format. While serving requests, decompresses and returns output.
+    /// </summary>
+    /// <typeparam name="TKey">Type of Input</typeparam>
+    /// <typeparam name="TValue">Type of value.</typeparam>
+    public class CompressedCache<TKey, TValue> : Cache<TKey, byte[]>, ICache<TKey, TValue>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompressedCache{TKey, TValue}"/> class.
+        /// </summary>
+        /// <param name="name">Input cache name.</param>
+        /// <param name="timetoLive">Input time to live.</param>
+        /// <param name="clock">Input clock.</param>
+        public CompressedCache(string name, TimeSpan timetoLive, ISystemClock clock) : base(name, timetoLive, clock)
+        {
+        }
+
+        /// <summary>
+        /// Get or add from cache.
+        /// </summary>
+        /// <param name="key">Input key</param>
+        /// <param name="valueFactory">Input value factory</param>
+        /// <returns>Value from cache</returns>
+        public virtual TValue GetOrAdd(TKey key, Func<TKey, TValue> valueFactory)
+        {
+            return this.GetOrAdd(key, valueFactory, this.DefaultTimeToRefresh);
+        }
+
+        /// <summary>
+        /// Get or add from cache.
+        /// </summary>
+        /// <param name="key">Input key</param>
+        /// <param name="valueFactory">Input value factory</param>
+        /// <param name="expirationTime">Input expiration time span</param>
+        /// <returns>Value from cache</returns>
+        public virtual TValue GetOrAdd(TKey key, Func<TKey, TValue> valueFactory, TimeSpan expirationTime)
+        {
+            var output = base.GetOrAdd(key, (k) => GzipCompression.Compress(JsonConvert.SerializeObject(valueFactory(k))), expirationTime);
+            return JsonConvert.DeserializeObject<TValue>(GzipCompression.Decompress(output));
+        }
+
+        /// <summary>
+        /// Get or add in an async manner.
+        /// </summary>
+        /// <param name="key">Input key</param>
+        /// <param name="valueFactory">Input value factory</param>
+        /// <param name="expirationTime">Input expiration time span.</param>
+        /// <returns>Value from cache.</returns>
+        public virtual async Task<TValue> GetOrAddAsync(TKey key, Func<TKey, Task<TValue>> valueFactory, TimeSpan expirationTime)
+        {
+            var output = await base.GetOrAddAsync(key, (k) => this.GetCompressedTask(k, valueFactory), expirationTime);
+            return JsonConvert.DeserializeObject<TValue>(GzipCompression.Decompress(output));
+        }
+
+        /// <summary>
+        /// Get compressed value task
+        /// </summary>
+        /// <param name="key">input key</param>
+        /// <param name="valueFactory">Input value factory task.</param>
+        /// <returns>Task returning compressed value.</returns>
+        private Task<byte[]> GetCompressedTask(TKey key, Func<TKey, Task<TValue>> valueFactory)
+        {
+            var fetchvalueTask = valueFactory(key);
+            var compressTask = fetchvalueTask.ContinueWith(t => GzipCompression.Compress(JsonConvert.SerializeObject(t.Result)));
+            return compressTask;
+        }
+    }
+}
---
+++ CompressedCache/GzipCompression.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace CompressedCache
+{
+    /// <summary>
+    /// Compress using GZIP.
+    /// </summary>
+    public class GzipCompression
+    {
+        /// <summary>
+        /// Decompress input string.
+        /// </summary>
+        /// <param name="compressed">Input compressed byte array.</param>
+        /// <returns>Decompressed string.</returns>
+        public static string Decompress(byte[] compressed)
+        {
+            byte[] decompressed = DecompressToBytes(compressed);
+            return Encoding.UTF8.GetString(decompressed);
+        }
+
+        /// <summary>
+        /// Compress input string.
+        /// </summary>
+        /// <param name="input">Input string.</param>
+        /// <returns>compressed byte array.</returns>
+        public static byte[] Compress(string input)
+        {
+            byte[] encoded = Encoding.UTF8.GetBytes(input);
+            byte[] compressed = CompressToBytes(encoded);
+            return compressed;
+        }
+
+        /// <summary>
+        /// Decompress input byte array
+        /// </summary>
+        /// <param name="input">Input byte array</param>
+        /// <returns>decompressed byte array</returns>
+        public static byte[] DecompressToBytes(byte[] input)
+        {
+            using (var source = new MemoryStream(input))
+            {
+                byte[] lengthBytes = new byte[4];
+                source.Read(lengthBytes, 0, 4);
+
+                var length = BitConverter.ToInt32(lengthBytes, 0);
+                using (var decompressionStream = new GZipStream(source, CompressionMode.Decompress))
+                {
+                    var result = new byte[length];
+                    decompressionStream.Read(result, 0, length);
+                    return result;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Compress input byte array
+        /// </summary>
+        /// <param name="input">Input byte array</param>
+        /// <returns>Compressed byte array</returns>
+        public static byte[] CompressToBytes(byte[] input)
+        {
+            using (var result = new MemoryStream())
+            {
+                var lengthBytes = BitConverter.GetBytes(input.Length);
+                result.Write(lengthBytes, 0, 4);
+
+                using (var compressionStream = new GZipStream(result, CompressionMode.Compress))
+                {
+                    compressionStream.Write(input, 0, input.Length);
+                    compressionStream.Flush();
+                }
+
+                return result.ToArray();
+            }
+        }
+    }
+}
